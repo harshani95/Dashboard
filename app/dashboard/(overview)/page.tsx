8188cd7ddbf756f8ddb407e624c192d23fdb285b
harshani <ruwanthikapemarathna@gmail.com>
2025-12-19 16:26:12 +0530

    Added streaming

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,11 +3,13 @@ import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
 import { fetchCardData } from "@/app/lib/data";
+import CardWrapper from "@/app/ui/dashboard/cards";
 
 import { Suspense } from "react";
 import {
   RevenueChartSkeleton,
   LatestInvoicesSkeleton,
+  CardsSkeleton,
 } from "@/app/ui/skeletons";
 
 export default async function Page() {
@@ -33,11 +35,8 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <Suspense fallback={<RevenueChartSkeleton />}>
-          <RevenueChart />
-        </Suspense>
-        <Suspense fallback={<LatestInvoicesSkeleton />}>
-          <LatestInvoices />
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
         </Suspense>
       </div>
     </main>
